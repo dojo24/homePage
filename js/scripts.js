ce3a513043c2d29b9685ce0bc9fd01231e8b5827
dojo24 <melissalongenberger24@gmail.com>
2021-07-08 20:58:59 -0400

    saving changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,29 @@
 $(document).ready(function(){
-    $('.dojo').click(function() {
-        $('#hideDojo').animate( {
+    $('#dojoEveryDay').click(function() {
+        $('#hideDojoEveryDay').animate( {
             width: 'toggle'
         })
-        $('#hideDojo').css({'display': 'flex',
+        $('#hideDojoEveryDay').css({'display': 'flex',
             'flex-direction': 'row',
             'justify-content': 'center',
             'align-items': 'flex-start'})
+    })
+    $('#dojoClass').click(function() {
+        $('#hideDojoClass').animate( {
+            width: 'toggle'
+        })
+        $('#hideDojoClass').css({'display': 'flex',
+            'flex-direction': 'row',
+            'justify-content': 'center',
+            'align-items': 'flex-start'})
+    })
+    $('#dojoOther').click(function() {
+        $('#hideDojoOther').animate( {
+            width: 'toggle'
+        })
+        $('#hideDojoOther').css({'display': 'flex',
+            'flex-direction': 'column',
+            'justify-content': 'center'})
     })
     $('.sirch').click(function() {
         $('#hideSirch').animate( {
