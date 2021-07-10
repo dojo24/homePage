2c1b2425721806a7c80c70fd1d97a5e16136ff03
dojo24 <melissalongenberger24@gmail.com>
2021-07-09 23:55:17 -0400

    saving new adjustments

--- js/scripts.js
+++ js/scripts.js
@@ -25,38 +25,92 @@ $(document).ready(function(){
             'flex-direction': 'column',
             'justify-content': 'center'})
     })
-    $('.sirch').click(function() {
-        $('#hideSirch').animate( {
+    $('#sirchFigma').click(function() {
+        $('#hideSirchFigma').animate( {
             width: 'toggle'
         })
-        $('#hideSirch').css({'display': 'flex',
+        $('#hideSirchFigma').css({'display': 'flex',
             'flex-direction': 'row',
             'justify-content': 'center',
             'align-items': 'flex-start'})
     })
-    $('.cfa').click(function() {
-        $('#hideCFA').animate( {
+    $('#sirchTasks').click(function() {
+        $('#hideSirchTasks').animate( {
             width: 'toggle'
         })
-        $('#hideCFA').css({'display': 'flex',
+        $('#hideSirchTasks').css({'display': 'flex',
             'flex-direction': 'row',
             'justify-content': 'center',
             'align-items': 'flex-start'})
     })
-    $('.personal').click(function() {
-        $('#hidePersonal').animate( {
+    $('#cfaMain').click(function() {
+        $('#hideCfaMain').animate( {
             width: 'toggle'
         })
-        $('#hidePersonal').css({'display': 'flex',
+        $('#hideCfaMain').css({'display': 'flex',
             'flex-direction': 'row',
             'justify-content': 'center',
             'align-items': 'flex-start'})
     })
-    $('.beedev').click(function() {
-        $('#hideBeeDev').animate( {
+    $('#cfaSites').click(function() {
+        $('#hideCfaSites').animate( {
             width: 'toggle'
         })
-        $('#hideBeeDev').css({'display': 'flex',
+        $('#hideCfaSites').css({'display': 'flex',
+            'flex-direction': 'row',
+            'justify-content': 'center',
+            'align-items': 'flex-start'})
+    })
+    $('#cfaDocs').click(function() {
+        $('#hideCfaDocs').animate( {
+            width: 'toggle'
+        })
+        $('#hideCfaDocs').css({'display': 'flex',
+            'flex-direction': 'row',
+            'justify-content': 'center',
+            'align-items': 'flex-start'})
+    })
+    $('#personalMain').click(function() {
+        $('#hidePersonalMain').animate( {
+            width: 'toggle'
+        })
+        $('#hidePersonalMain').css({'display': 'flex',
+            'flex-direction': 'row',
+            'justify-content': 'center',
+            'align-items': 'flex-start'})
+    })
+    $('#personalSites').click(function() {
+        $('#hidePersonalSites').animate( {
+            width: 'toggle'
+        })
+        $('#hidePersonalSites').css({'display': 'flex',
+            'flex-direction': 'row',
+            'justify-content': 'center',
+            'align-items': 'flex-start'})
+    })
+    $('#personalLinks').click(function() {
+        $('#hidePersonalLinks').animate( {
+            width: 'toggle'
+        })
+        $('#hidePersonalLinks').css({'display': 'flex',
+            'flex-direction': 'row',
+            'justify-content': 'center',
+            'align-items': 'flex-start'})
+    })
+    $('#beeDevMain').click(function() {
+        $('#hideBeeDevMain').animate( {
+            width: 'toggle'
+        })
+        $('#hideBeeMain').css({'display': 'flex',
+        'flex-direction': 'row',
+        'justify-content': 'center',
+        'align-items': 'flex-start'})
+    })
+    $('#beeDevLinks').click(function() {
+        $('#hideBeeDevLinks').animate( {
+            width: 'toggle'
+        })
+        $('#hideBeeLinks').css({'display': 'flex',
         'flex-direction': 'row',
         'justify-content': 'center',
         'align-items': 'flex-start'})
