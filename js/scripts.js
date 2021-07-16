b3964e5fa737489a79ae8617e330fbca9ef3dce9
Melissa Longenberger <mlongenberger@codingdojo.com>
2021-07-15 23:16:26 -0400

    added more links

--- js/scripts.js
+++ js/scripts.js
@@ -3,170 +3,230 @@ $(document).ready(function(){
         $('#hideDojoEveryDay').animate( {
             width: 'toggle'
         })
-        $('#hideDojoEveryDay').css({'display': 'flex',
+        $('#hideDojoEveryDay').css({
+            'display': 'flex',
             'flex-direction': 'row',
             'justify-content': 'center',
-            'align-items': 'flex-start'})
+            'align-items': 'flex-start'
+        })
     })
     $('#dojoClass').click(function() {
         $('#hideDojoClass').animate( {
             width: 'toggle'
         })
-        $('#hideDojoClass').css({'display': 'flex',
+        $('#hideDojoClass').css({
+            'display': 'flex',
             'flex-direction': 'row',
             'justify-content': 'center',
-            'align-items': 'flex-start'})
+            'align-items': 'flex-start'
+        })
     })
     $('#dojoOther').click(function() {
         $('#hideDojoOther').animate( {
             width: 'toggle'
         })
-        $('#hideDojoOther').css({'display': 'flex',
+        $('#hideDojoOther').css({
+            'display': 'flex',
             'flex-direction': 'column',
-            'justify-content': 'center'})
+            'justify-content': 'center'
+        })
     })
     $('#sirchFigma').click(function() {
         $('#hideSirchFigma').animate( {
             width: 'toggle'
         })
-        $('#hideSirchFigma').css({'display': 'flex',
+        $('#hideSirchFigma').css({
+            'display': 'flex',
             'flex-direction': 'column',
             'justify-content': 'center',
-            'align-items': 'flex-start'})
+            'align-items': 'flex-start'
+        })
     })
     $('#sirchTasks').click(function() {
         $('#hideSirchTasks').animate( {
             width: 'toggle'
         })
-        $('#hideSirchTasks').css({'display': 'flex',
+        $('#hideSirchTasks').css({
+            'display': 'flex',
             'flex-direction': 'column',
             'justify-content': 'center',
-            'align-items': 'flex-start'})
+            'align-items': 'flex-start'
+        })
     })
     $('#cfaMain').click(function() {
         $('#hideCfaMain').animate( {
             width: 'toggle'
         })
-        $('#hideCfaMain').css({'display': 'flex',
+        $('#hideCfaMain').css({
+            'display': 'flex',
             'flex-direction': 'column',
             'justify-content': 'center',
-            'align-items': 'flex-start'})
+            'align-items': 'flex-start'
+        })
     })
     $('#cfaSites').click(function() {
         $('#hideCfaSites').animate( {
             width: 'toggle'
         })
-        $('#hideCfaSites').css({'display': 'flex',
+        $('#hideCfaSites').css({
+            'display': 'flex',
             'flex-direction': 'column',
             'justify-content': 'center',
-            'align-items': 'flex-start'})
+            'align-items': 'flex-start'
+        })
     })
     $('#cfaDocs').click(function() {
         $('#hideCfaDocs').animate( {
             width: 'toggle'
         })
-        $('#hideCfaDocs').css({'display': 'flex',
+        $('#hideCfaDocs').css({
+            'display': 'flex',
             'flex-direction': 'column',
             'justify-content': 'center',
-            'align-items': 'flex-start'})
+            'align-items': 'flex-start'
+        })
     })
     $('#personalMain').click(function() {
         $('#hidePersonalMain').animate( {
             width: 'toggle'
         })
-        $('#hidePersonalMain').css({'display': 'flex',
+        $('#hidePersonalMain').css({
+            'display': 'flex',
             'flex-direction': 'column',
             'justify-content': 'center',
-            'align-items': 'flex-start'})
+            'align-items': 'flex-start'
+        })
     })
     $('#personalSites').click(function() {
         $('#hidePersonalSites').animate( {
             width: 'toggle'
         })
-        $('#hidePersonalSites').css({'display': 'flex',
+        $('#hidePersonalSites').css({
+            'display': 'flex',
             'flex-direction': 'column',
             'justify-content': 'center',
-            'align-items': 'flex-start'})
+            'align-items': 'flex-start'
+        })
     })
     $('#personalLinks').click(function() {
         $('#hidePersonalLinks').animate( {
             width: 'toggle'
         })
-        $('#hidePersonalLinks').css({'display': 'flex',
+        $('#hidePersonalLinks').css({
+            'display': 'flex',
             'flex-direction': 'column',
             'justify-content': 'center',
-            'align-items': 'flex-start'})
+            'align-items': 'flex-start'
+        })
     })
     $('#beeDevMain').click(function() {
         $('#hideBeeDevMain').animate( {
             width: 'toggle'
         })
-        $('#hideBeeMain').css({'display': 'flex',
-        'flex-direction': 'column',
-        'justify-content': 'center',
-        'align-items': 'flex-start'})
+        $('#hideBeeMain').css({
+            'display': 'flex',
+            'flex-direction': 'column',
+            'justify-content': 'center',
+            'align-items': 'flex-start'
+        })
     })
     $('#beeDevLinks').click(function() {
         $('#hideBeeDevLinks').animate( {
             width: 'toggle'
         })
-        $('#hideBeeLinks').css({'display': 'flex',
-        'flex-direction': 'column',
-        'justify-content': 'center',
-        'align-items': 'flex-start'})
+        $('#hideBeeLinks').css({
+            'display': 'flex',
+            'flex-direction': 'column',
+            'justify-content': 'center',
+            'align-items': 'flex-start'
+        })
     })
-    $('#lambdaNotes').click(function() {
-        $('#hideLambdaNotes').animate( {
+    $('#webDevNotes').click(function() {
+        $('#hideWebDevNotes').animate( {
             width: 'toggle'
         })
-        $('#hideLambdaNotes').css({'display': 'flex',
-        'flex-direction': 'column',
-        'justify-content': 'center',
-        'align-items': 'flex-start'})
+        $('#hideWebDevNotes').css({
+            'display': 'flex',
+            'flex-direction': 'column',
+            'justify-content': 'center',
+            'align-items': 'flex-start'
+        })
     })
     $('#lambdaTk').click(function() {
         $('#hideLambdaTk').animate( {
             width: 'toggle'
         })
-        $('#hideLambdaTk').css({'display': 'flex',
-        'flex-direction': 'column',
-        'justify-content': 'center',
-        'align-items': 'flex-start'})
+        $('#hideLambdaTk').css({
+            'display': 'flex',
+            'flex-direction': 'column',
+            'justify-content': 'center',
+            'align-items': 'flex-start'
+        })
     })
     $('#nexGenT').click(function() {
         $('#hideNexGenT').animate( {
             width: 'toggle'
         })
-        $('#hideNexGenT').css({'display': 'flex',
-        'flex-direction': 'column',
-        'justify-content': 'center',
-        'align-items': 'flex-start'})
+        $('#hideNexGenT').css({
+            'display': 'flex',
+            'flex-direction': 'column',
+            'justify-content': 'center',
+            'align-items': 'flex-start'
+        })
     })
-    $('#otherCourses').click(function() {
-        $('#hideOtherCourses').animate( {
+    $('#webDevCourses').click(function() {
+        $('#hideWebDevCourses').animate( {
             width: 'toggle'
         })
-        $('#hideOtherCourses').css({'display': 'flex',
-        'flex-direction': 'column',
-        'justify-content': 'center',
-        'align-items': 'flex-start'})
+        $('#hideWebDevCourses').css({
+            'display': 'flex',
+            'flex-direction': 'column',
+            'justify-content': 'center',
+            'align-items': 'flex-start'
+        })
+    })
+    $('#microsoftCourses').click(function() {
+        $('#hideMicrosoftCourses').animate( {
+            width: 'toggle'
+        })
+        $('#hideMicrosoftCourses').css({
+            'display': 'flex',
+            'flex-direction': 'column',
+            'justify-content': 'center',
+            'align-items': 'flex-start'
+    })
+    })
+    $('#otherNotes').click(function() {
+        $('#hideOtherNotes').animate( {
+            width: 'toggle'
+        })
+        $('#hideOtherNotes').css({
+            'display': 'flex',
+            'flex-direction': 'column',
+            'justify-content': 'center',
+            'align-items': 'flex-start'
+        })
     })
     $('#challenges').click(function() {
         $('#hideChallenges').animate( {
             width: 'toggle'
         })
-        $('#hideChallenges').css({'display': 'flex',
-        'flex-direction': 'column',
-        'justify-content': 'center',
-        'align-items': 'flex-start'})
+        $('#hideChallenges').css({
+            'display': 'flex',
+            'flex-direction': 'column',
+            'justify-content': 'center',
+            'align-items': 'flex-start'
+    })
     })
     $('#ide').click(function() {
         $('#hideIde').animate( {
             width: 'toggle'
         })
-        $('#hideIde').css({'display': 'flex',
-        'flex-direction': 'column',
-        'justify-content': 'center',
-        'align-items': 'flex-start'})
+        $('#hideIde').css({
+            'display': 'flex',
+            'flex-direction': 'column',
+            'justify-content': 'center',
+            'align-items': 'flex-start'
+        })
     })
 })
