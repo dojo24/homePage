c4affa7c4cfc7d47bfff07c27357c9bd69e27554
dojo24 <melissalongenberger24@gmail.com>
2021-09-30 20:59:59 -0400

    huge overhaul....not done till txt is empty

--- js/scripts.js
+++ js/scripts.js
@@ -1,335 +1,46 @@
 $(document).ready(function(){
-    // codingdojo.html line 29
-    $('#dojoClassGeneral').click(function() {
-        $('#hideDojoClassGeneral').animate( {
+    $('#showWork').click(function() {
+        $('#hideWork').animate( {
             width: 'toggle'
         })
-        $('#hideDojoClassGeneral').css({
+        $('#hideWork').css({
             'display': 'flex',
             'flex-direction': 'column',
             'justify-content': 'center',
             'align-items': 'center'
         })
     })
-    // codingdojo.html line 44
-    $('#dojoWebClass').click(function() {
-        $('#hideDojoWebClass').animate( {
+    $('#showLearning').click(function() {
+        $('#hideLearning').animate( {
             width: 'toggle'
         })
-        $('#hideDojoWebClass').css({
+        $('#hideLearning').css({
             'display': 'flex',
             'flex-direction': 'column',
             'justify-content': 'center',
             'align-items': 'center'
         })
     })
-        // codingdojo.html line 52
-    $('#dojoPythonClass').click(function() {
-        $('#hideDojoPythonClass').animate( {
+    $('#showHome').click(function() {
+        $('#hideHome').animate( {
             width: 'toggle'
         })
-        $('#hideDojoPythonClass').css({
+        $('#hideHome').css({
             'display': 'flex',
             'flex-direction': 'column',
             'justify-content': 'center',
             'align-items': 'center'
         })
     })
-    // codingdojo.html line 65
-    $('#dojoAdminDrive').click(function() {
-        $('#hideDojoAdminDrive').animate( {
+    $('#showCoding').click(function() {
+        $('#hideCoding').animate( {
             width: 'toggle'
         })
-        $('#hideDojoAdminDrive').css({
+        $('#hideCoding').css({
             'display': 'flex',
             'flex-direction': 'column',
             'justify-content': 'center',
             'align-items': 'center'
-        })
-    })
-    // codingdojo.html line75
-    $('#dojoAdminCalendar').click(function() {
-        $('#hideDojoAdminCalendar').animate( {
-            width: 'toggle'
-        })
-        $('#hideDojoAdminCalendar').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'center'
-        })
-    })
-    // codingdojo.html line 88
-    $('#dojoMonthly').click(function() {
-        $('#hideDojoMonthly').animate({
-            width: 'toggle'
-        })
-        $('#hideDojoMonthly').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'center'
-        })
-    })
-    // codingdojo.html line 96
-    $('#dojoOther').click(function() {
-        $('#hideDojoOther').animate({
-            width: 'toggle'
-        })
-        $('#hideDojoOther').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'center'
-        })
-    })
-    // work.html line 32
-    $('#sirchFigma').click(function() {
-        $('#hideSirchFigma').animate( {
-            width: 'toggle'
-        })
-        $('#hideSirchFigma').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'center'
-        })
-    })
-    // work.html line 40
-    $('#sirchTasks').click(function() {
-        $('#hideSirchTasks').animate( {
-            width: 'toggle'
-        })
-        $('#hideSirchTasks').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'center'
-        })
-    })
-    // work.html line 48
-    $('#sirchLinks').click(function() {
-        $('#hideSirchLinks').animate( {
-            width: 'toggle'
-        })
-        $('#hideSirchLinks').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'center'
-        })
-    })
-    // volunteer.html line26
-    $('#cfaMain').click(function() {
-        $('#hideCfaMain').animate( {
-            width: 'toggle'
-        })
-        $('#hideCfaMain').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'center'
-        })
-    })
-    // volunteer.html line 32
-    $('#cfaSites').click(function() {
-        $('#hideCfaSites').animate( {
-            width: 'toggle'
-        })
-        $('#hideCfaSites').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'center'
-        })
-    })
-    // volunteer.html line 41
-    $('#cfaDrive').click(function() {
-        $('#hideCfaDrive').animate( {
-            width: 'toggle'
-        })
-        $('#hideCfaDrive').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'center'
-        })
-    })
-    // volunteer.html line 47
-    $('#cfaDocs').click(function() {
-        $('#hideCfaDocs').animate( {
-            width: 'toggle'
-        })
-        $('#hideCfaDocs').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'center'
-        })
-    })
-    $('#personalMain').click(function() {
-        $('#hidePersonalMain').animate( {
-            width: 'toggle'
-        })
-        $('#hidePersonalMain').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'flex-start'
-        })
-    })
-    $('#personalSites').click(function() {
-        $('#hidePersonalSites').animate( {
-            width: 'toggle'
-        })
-        $('#hidePersonalSites').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'flex-start'
-        })
-    })
-    $('#personalLinks').click(function() {
-        $('#hidePersonalLinks').animate( {
-            width: 'toggle'
-        })
-        $('#hidePersonalLinks').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'flex-start'
-        })
-    })
-    $('#medicalSites').click(function() {
-        $('#hideMedicalSites').animate( {
-            width: 'toggle'
-        })
-        $('#hideMedicalSites').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'flex-start'
-        })
-    })
-    $('#vaSites').click(function() {
-        $('#hideVaSites').animate( {
-            width: 'toggle'
-        })
-        $('#hideVaSites').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'flex-start'
-        })
-    })
-    $('#beeDevMain').click(function() {
-        $('#hideBeeDevMain').animate( {
-            width: 'toggle'
-        })
-        $('#hideBeeMain').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'flex-start'
-        })
-    })
-    $('#beeDevLinks').click(function() {
-        $('#hideBeeDevLinks').animate( {
-            width: 'toggle'
-        })
-        $('#hideBeeLinks').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'flex-start'
-        })
-    })
-    $('#webDevNotes').click(function() {
-        $('#hideWebDevNotes').animate( {
-            width: 'toggle'
-        })
-        $('#hideWebDevNotes').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'flex-start'
-        })
-    })
-    $('#lambdaTk').click(function() {
-        $('#hideLambdaTk').animate( {
-            width: 'toggle'
-        })
-        $('#hideLambdaTk').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'flex-start'
-        })
-    })
-    $('#nexGenT').click(function() {
-        $('#hideNexGenT').animate( {
-            width: 'toggle'
-        })
-        $('#hideNexGenT').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'flex-start'
-        })
-    })
-    $('#webDevCourses').click(function() {
-        $('#hideWebDevCourses').animate( {
-            width: 'toggle'
-        })
-        $('#hideWebDevCourses').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'flex-start'
-        })
-    })
-    $('#microsoftCourses').click(function() {
-        $('#hideMicrosoftCourses').animate( {
-            width: 'toggle'
-        })
-        $('#hideMicrosoftCourses').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'flex-start'
-    })
-    })
-    $('#otherNotes').click(function() {
-        $('#hideOtherNotes').animate( {
-            width: 'toggle'
-        })
-        $('#hideOtherNotes').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'flex-start'
-        })
-    })
-    $('#challenges').click(function() {
-        $('#hideChallenges').animate( {
-            width: 'toggle'
-        })
-        $('#hideChallenges').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'flex-start'
-    })
-    })
-    $('#ide').click(function() {
-        $('#hideIde').animate( {
-            width: 'toggle'
-        })
-        $('#hideIde').css({
-            'display': 'flex',
-            'flex-direction': 'column',
-            'justify-content': 'center',
-            'align-items': 'flex-start'
         })
     })
 })
